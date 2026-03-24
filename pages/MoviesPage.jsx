7dbc40a5795ab685f69eaa08236ff9299ab66437
Christian Tondo <christiantondo1@gmail.com>
2026-03-24 02:18:36 +0100

    cards in homepage and style

--- pages/MoviesPage.jsx
+++ pages/MoviesPage.jsx
@@ -1,11 +1,47 @@
-function MoviesPage() {
-    return <>
-        <h1>All Movies</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
+import { Link } from "react-router-dom";
 
-        <h2>Our movies</h2>
-        { /* Movie list */}
-    </>
+function MoviesPage() {
+	return <>
+		<h1>All Movies</h1>
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
+
+		<div className="cards-container">
+			<div className="card">
+				<h2>Title</h2>
+				<h3>Director</h3>
+				<p>Abstract</p>
+				<Link to={`/movies/1`}>Other details</Link>
+			</div>
+
+			<div className="card">
+				<h2>Title</h2>
+				<h3>Director</h3>
+				<p>Abstract</p>
+				<Link to={`/movies/1`}>Other details</Link>
+			</div>
+
+			<div className="card">
+				<h2>Title</h2>
+				<h3>Director</h3>
+				<p>Abstract</p>
+				<Link to={`/movies/1`}>Other details</Link>
+			</div>
+
+			<div className="card">
+				<h2>Title</h2>
+				<h3>Director</h3>
+				<p>Abstract</p>
+				<Link to={`/movies/1`}>Other details</Link>
+			</div>
+
+			<div className="card">
+				<h2>Title</h2>
+				<h3>Director</h3>
+				<p>Abstract</p>
+				<Link to={`/movies/1`}>Other details</Link>
+			</div>
+		</div>
+	</>
 }
 
 export default MoviesPage;
