a22ac27fa7388d747d05ee4a12520ec59f54adee
Christian Tondo <christiantondo1@gmail.com>
2026-03-24 02:54:46 +0100

    inserted movie placeholder in moviespage for testing

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,12 +1,21 @@
 import MovieCard from "../components/MovieCard";
 
 function MoviesPage() {
+
+    const moviePlaceHolder = {
+        id: 99,
+        title: "Blank",
+        director: "Mike",
+        abstract: "Lorem ipsum dolor sit amet",
+        image: "/directory/x.jpg"
+    };
+
     return <>
         <h1>All Movies</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
 
         <div className="cards-container">
-            <MovieCard />
+            <MovieCard movie={moviePlaceHolder} />
         </div>
     </>
 }
