073abfe55b8fe1aceb978ac8d8036035f25b9a50
Christian Tondo <christiantondo1@gmail.com>
2026-03-24 02:45:11 +0100

    added link to go back to full list

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom";
+
 function MoviePage() {
     return <>
+        <Link to="/movies">Go back to full list</Link>
         <h1>Single movie page</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
     </>
