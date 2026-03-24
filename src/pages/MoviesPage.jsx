ed215a1eb55908a8fdaad48e907f0fa9fb609100
Christian Tondo <christiantondo1@gmail.com>
2026-03-24 02:29:50 +0100

    moviecard component imported

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,3 +1,4 @@
+import MovieCard from "../components/MovieCard";
 
 function MoviesPage() {
     return <>
@@ -5,7 +6,7 @@ function MoviesPage() {
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
 
         <div className="cards-container">
-
+            <MovieCard />
         </div>
     </>
 }
