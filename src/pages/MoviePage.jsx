6df4b4e35077976d459bbee1dad10a10360daef0
Christian Tondo <christiantondo1@gmail.com>
2026-03-30 18:51:38 +0200

    placeholder removed: now we get reviews from db

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,25 +1,36 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 import ReviewCard from "../components/ReviewCard";
 
-const reviewPlaceHolder = {
-    name: "Mario Rossi",
-    vote: 4,
-    text: "lorem ipsum dolor"
-};
-
-const reviews = [
-    reviewPlaceHolder,
-    reviewPlaceHolder
-]
-
 function MoviePage() {
+
+    const { id } = useParams();
+
+    const [movie, setMovie] = useState({
+        title: "",
+        abstract: "",
+        reviews: []
+    });
+
+    useEffect(() => {
+
+        console.log("Showing movie id:", id);
+
+        axios.get(`http://localhost:3000/api/movies/${id}`).then(res => {
+            console.log(res.data);
+            setMovie(res.data);
+        }).catch(err =>
+            console.error("Ops...", err.message));
+    }, []);
+
     return <>
         <Link to="/movies">Go back to full list</Link>
-        <h1>Single movie page</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
+        <h1>{movie.title}</h1>
+        <p>{movie.abstract}</p>
 
         <div className="cards-container">
-            {reviews.map(review => <ReviewCard review={review} />)}
+            {movie.reviews.map(review => <ReviewCard key={review.id} review={review} />)}
         </div>
     </>
 }
