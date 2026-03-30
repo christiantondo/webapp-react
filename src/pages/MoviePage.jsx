0181345f697fe680289c5821c9f604240db19678
Christian Tondo <christiantondo1@gmail.com>
2026-03-30 17:23:32 +0200

    ReviewCard component imported and map on placeholder array

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,10 +1,26 @@
 import { Link } from "react-router-dom";
+import ReviewCard from "../components/ReviewCard";
+
+const reviewPlaceHolder = {
+    name: "Mario Rossi",
+    vote: 4,
+    text: "lorem ipsum dolor"
+};
+
+const reviews = [
+    reviewPlaceHolder,
+    reviewPlaceHolder
+]
 
 function MoviePage() {
     return <>
         <Link to="/movies">Go back to full list</Link>
         <h1>Single movie page</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
+
+        <div className="cards-container">
+            {reviews.map(review => <ReviewCard review={review} />)}
+        </div>
     </>
 }
 
