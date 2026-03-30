f36c0312898f02869abbb924fd67eb1f37d6f9df
Christian Tondo <christiantondo1@gmail.com>
2026-03-30 17:50:38 +0200

    axios get

--- src/pages/MoviesListPage.jsx
+++ src/pages/MoviesListPage.jsx
@@ -1,28 +1,37 @@
+import { useEffect } from "react";
 import MovieCard from "../components/MovieCard";
+import axios from "axios";
 
 function MoviesPage() {
 
-    const moviePlaceHolder = {
-        id: 99,
-        title: "Blank",
-        director: "Mike",
-        abstract: "Lorem ipsum dolor sit amet",
-        image: "/directory/x.jpg"
-    };
+	const moviePlaceHolder = {
+		id: 99,
+		title: "Blank",
+		director: "Mike",
+		abstract: "Lorem ipsum dolor sit amet",
+		image: "/directory/x.jpg"
+	};
 
-    const movies = [
-        moviePlaceHolder,
-        moviePlaceHolder
-    ]
+	const movies = [
+		moviePlaceHolder,
+		moviePlaceHolder
+	];
 
-    return <>
-        <h1>All Movies</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
+	useEffect(() => {
+		axios.get("http://localhost:3000/api/movies").then(res => {
+			console.log(res.data);
+		}).catch(err =>
+			console.error("Ops...", err.message));
+	}, []);
 
-        <div className="cards-container">
-            {movies.map(movie => <MovieCard movie={movie} />)}
-        </div>
-    </>
+	return <>
+		<h1>All Movies</h1>
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
+
+		<div className="cards-container">
+			{movies.map(movie => <MovieCard movie={movie} />)}
+		</div>
+	</>
 }
 
 export default MoviesPage;
