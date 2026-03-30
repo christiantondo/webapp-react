3a141395ce39099f9d6286b89fd92456cb22aea5
Christian Tondo <christiantondo1@gmail.com>
2026-03-30 17:22:47 +0200

    created new component "ReviewCard"

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,13 @@
+function ReviewCard({ review }) {
+
+    const { name, vote, text } = review;
+
+    return <div className="card">
+        <h2>{name}</h2>
+        <h3>{vote}</h3>
+        <p>{text}</p>
+    </div>
+
+}
+
+export default ReviewCard;
