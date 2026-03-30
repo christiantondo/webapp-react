987bad3037bb7d1639e403f9f739aaa0024bcf1a
Christian Tondo <christiantondo1@gmail.com>
2026-03-30 16:18:13 +0200

    map on movies (placeholder) array

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 function MovieCard({ movie }) {
 
-	const { id, title, director, abstract, image } = movie;
+    const { id, title, director, abstract, image } = movie;
 
-	return <div className="card">
-		<img src={image} alt={title} />
-		<h2>{title}</h2>
-		<h3>{director}</h3>
-		<p>{abstract}</p>
-		<Link to={`/movies/${id}`}>Other details</Link>
-	</div>
+    return <div className="card">
+        <img src={image} alt={title} />
+        <h2>{title}</h2>
+        <h3>{director}</h3>
+        <p>{abstract}</p>
+        <Link to={`/movies/${id}`}>Other details</Link>
+    </div>
 
 }
 
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -10,12 +10,17 @@ function MoviesPage() {
         image: "/directory/x.jpg"
     };
 
+    const movies = [
+        moviePlaceHolder,
+        moviePlaceHolder
+    ]
+
     return <>
         <h1>All Movies</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, unde!</p>
 
         <div className="cards-container">
-            <MovieCard movie={moviePlaceHolder} />
+            {movies.map(movie => <MovieCard movie={movie} />)}
         </div>
     </>
 }
